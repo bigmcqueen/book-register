335b976f7d0483ec1f4879b5ebee18748fd64ba5
BigMcQueen <mmmrym10@gmail.com>
2023-01-22 13:45:20 +0900

    Submissions can be saved

--- db/migrate/20230122033247_create_posts.rb
+++ db/migrate/20230122033247_create_posts.rb
@@ -1,7 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.text :content
+      t.string :title
+      t.text :thoughts
 
       t.timestamps
     end
