862c07015be0e826868c1773266d114c3a68527e
BigMcQueen <mmmrym10@gmail.com>
2023-01-24 00:32:15 +0900

    Create User function

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(name: params[:name], email: params[:email])
+    if @user.save
+      flash[:notice] = 'Registration Complet!'
+      render 'show'
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    if @user.save
+      flash[:notice] = 'Edited User Information!'
+      render 'show'
+    else
+      render 'edit'
+    end
+  end
+end
