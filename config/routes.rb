3ea4f72dda4a5b9e903064d81a1ad5a44ea5d295
BigMcQueen <mmmrym10@gmail.com>
2023-01-27 19:06:17 +0900

    create home, about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'records/index'
+  get 'records/show'
+  get 'records/new'
+  get 'records/edit'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/' => 'home#top'
+  get 'about' => 'about'
 end
