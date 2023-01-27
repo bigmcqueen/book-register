f643955a447032c918f1527faa5984799c1bd6f2
BigMcQueen <mmmrym10@gmail.com>
2023-01-27 23:45:34 +0900

    Add edit and delete functions

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -11,6 +11,33 @@ class RecordsController < ApplicationController
     @record = Record.new
   end
 
+  def create
+    @record = Record.new(
+      title: params[:title],
+      thoughts: params[:thoughts],
+      )
+    @record.save
+
+    redirect_to "/records/index"
+  end
+
   def edit
+    @record = Record.find_by(id: params[:id])
+  end
+
+  def update
+    @record = Record.find_by(id: params[:id])
+    @record.title = params[:title]
+    @record.thoughts = params[:thoughts]
+    @record.save
+
+    redirect_to "/records/index"
+  end
+
+  def destroy
+    @record = Record.find_by(id: params[:id])
+    @record.destroy
+    
+    redirect_to "/records/index"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'records/index' => 'records#index'
-  get 'records/:id' => 'records#show'
+  get 'records/index'
   get 'records/new'
-  get 'records/edit'
+  get 'records/:id' => 'records#show'
+  post 'records/create'
+  get 'records/:id/edit' => 'records#edit'
+  post 'records/:id/update' => 'records#update'
+  post 'records/:id/destroy' => 'records#destroy'
 
   get '/' => 'home#top'
   get 'about' => 'home#about'
