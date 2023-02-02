50bc727d660565a2a5486272cffd855951dc4b7b
BigMcQueen <mmmrym10@gmail.com>
2023-02-02 23:10:45 +0900

    Hashing of passwords

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_01_114320) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_02_140254) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_01_114320) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
